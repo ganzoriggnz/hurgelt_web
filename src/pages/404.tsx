e0baa513bed0d5d95d9cbeb962667dffa61e0431
GanzorigB <ganzoriggnz@gmail.com>
2024-07-31 23:23:54 +0800

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 const PageNotFound = () => (
   <Link href="/">
-  <div className="text-[45px] text-blue-950 w-full h-screen flex items-center justify-center flex-col ">
-    <p className="text-[85px] ">404</p>
-    <p>Энэ хуудас байхгүй байна!!!</p>
-  </div>
+    <div className="text-[45px] text-blue-950 w-full h-screen flex items-center justify-center flex-col ">
+      <p className="text-[85px] ">404</p>
+      <p>Энэ хуудас байхгүй байна!!! *</p>
+    </div>
   </Link>
 );
 PageNotFound.hideHeader = true;
